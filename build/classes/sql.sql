dd3bf2c9f9118d8f8f8e9c737e908e04506fb132
Victor Gonçalves <victorvaz001@gmail.com>
2020-05-09 14:50:31 -0300

    Adicionado arquivo de SQL

--- build/classes/sql.sql
+++ build/classes/sql.sql
@@ -1,5 +1,17 @@
 ---------------------------------------------------------------------------
+//Banco de dados
+
+CREATE DATABASE "curso-jsp"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Portuguese_Brazil.1252'
+       LC_CTYPE = 'Portuguese_Brazil.1252'
+       CONNECTION LIMIT = -1;
+
+---------------------------------------------------------------------------
 //Tabela de Produto
+
 create table produto(
 id bigint not null,
 nome character varying(255),
@@ -9,6 +21,7 @@ constraint prod_id primary key(id)
 );
 ---------------------------------------------------------------------------
 //Tabela de telefones
+
 CREATE TABLE public.telefone
 (
   id bigint NOT NULL DEFAULT nextval('fonesequence'::regclass),
@@ -24,6 +37,7 @@ ALTER TABLE public.telefone
   OWNER TO postgres;
 ---------------------------------------------------------------------------
 //Tabela de categoria
+
 CREATE TABLE public.categoria
 (
   id bigint NOT NULL DEFAULT nextval('categoriasequence'::regclass),
@@ -37,6 +51,37 @@ ALTER TABLE public.categoria
   OWNER TO postgres;
 ---------------------------------------------------------------------------
 
+//Tabela de usuario
+CREATE TABLE public.usuario
+(
+  login character varying,
+  senha character varying,
+  id bigint NOT NULL DEFAULT nextval('serialuser'::regclass),
+  nome character varying(500),
+  cep character varying(200),
+  rua character varying(200),
+  bairro character varying(200),
+  cidade character varying(200),
+  estado character varying(200),
+  ibge character varying(200),
+  fotobase64 text,
+  contenttype text,
+  contenttyoecurriculo text,
+  curriculobase64 text,
+  fotobase64miniatura text,
+  ativo boolean,
+  sexo character varying(50),
+  perfil character varying(20),
+  imagem text
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.usuario
+  OWNER TO postgres;
+
+---------------------------------------------------------------------------
+Quary em postgres
  
 ALTER TABLE public.usuario ADD COLUMN nome character varying(500);
 ALTER TABLE public.usuario
